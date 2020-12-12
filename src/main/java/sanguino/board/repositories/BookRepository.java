7eba605d1b01d7649774805d964dd4593cb01df7
Miguel G. Sanguino <sanguino@me.com>
2020-12-12 19:37:33 +0100

    create jpa repositories for book and comments

--- src/main/java/sanguino/board/repositories/BookRepository.java
+++ src/main/java/sanguino/board/repositories/BookRepository.java
@@ -1,36 +1,8 @@
 package sanguino.board.repositories;
 
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import sanguino.board.model.Book;
 
-import java.util.Collection;
-import java.util.Optional;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentMap;
-import java.util.concurrent.atomic.AtomicLong;
+public interface BookRepository extends JpaRepository<Book, Long> {
 
-@Repository
-public class BookRepository {
-
-    private ConcurrentMap<Long, Book> books = new ConcurrentHashMap<>();
-    private AtomicLong nextId = new AtomicLong();
-
-    public BookRepository() {
-        this.save(new Book("Guia del autoestopista galactico", "un buen libro", "Douglas Adams", "Anagrama", 1979));
-    }
-
-    public Collection<Book> findAll() {
-        return this.books.values();
-    }
-
-    public Book save(Book book) {
-        long id = this.nextId.getAndIncrement();
-        book.setId(id);
-        this.books.put(id, book);
-        return book;
-    }
-
-    public Optional<Book> findById(long id) {
-        return Optional.ofNullable(this.books.get(id));
-    }
 }
--- src/main/java/sanguino/board/repositories/CommentRepository.java
+++ src/main/java/sanguino/board/repositories/CommentRepository.java
@@ -1,47 +1,8 @@
 package sanguino.board.repositories;
 
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import sanguino.board.model.Comment;
 
-import java.util.List;
-import java.util.Optional;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentMap;
-import java.util.concurrent.atomic.AtomicLong;
-import java.util.stream.Collectors;
+public interface CommentRepository extends JpaRepository<Comment, Long> {
 
-@Repository
-public class CommentRepository {
-
-    private ConcurrentMap<Long, Comment> comments = new ConcurrentHashMap<>();
-    private AtomicLong nextId = new AtomicLong();
-
-    public CommentRepository() {
-        this.addComment(new Comment(0L, "Peter", "Great Book", 42));
-    }
-
-    public Comment addComment(Comment comment) {
-        long id = this.nextId.getAndIncrement();
-        comment.setId(id);
-        this.comments.put(id, comment);
-        return comment;
-    }
-
-    public List<Comment> findByBookId(long id) {
-        return comments.values().stream()
-                .filter(comment -> comment.getBookId() == id)
-                .collect(Collectors.toList());
-    }
-
-    public Optional<Comment> findByBookIdAndId(long bookId, long id) {
-        Comment comment = comments.get(id);
-        if (comment != null && comment.getBookId() == bookId) {
-            return Optional.ofNullable(comment);
-        }
-        return Optional.empty();
-    }
-
-    public Comment deleteCommentById(long id) {
-        return this.comments.remove(id);
-    }
 }
