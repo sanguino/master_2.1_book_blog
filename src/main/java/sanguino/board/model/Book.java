9950ba2947160f7a172b67ebbfe26ac3800fcc18
Miguel G. Sanguino <sanguino@me.com>
2020-12-12 19:36:31 +0100

    convert models in entities

--- src/main/java/sanguino/board/model/Book.java
+++ src/main/java/sanguino/board/model/Book.java
@@ -1,9 +1,16 @@
 package sanguino.board.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.util.Collection;
 import java.util.List;
 
+@Entity
 public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String title;
     private String synopsis;
--- src/main/java/sanguino/board/model/Comment.java
+++ src/main/java/sanguino/board/model/Comment.java
@@ -1,7 +1,15 @@
 package sanguino.board.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Comment {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private Long bookId;
     private String name;
