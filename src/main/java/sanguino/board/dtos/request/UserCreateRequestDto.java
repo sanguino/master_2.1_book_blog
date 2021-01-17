120064b52cd13b5113849acc2ed7fea60be2cb9d
Miguel G. Sanguino <sanguino@me.com>
2021-01-17 17:07:37 +0100

    added password to CreateRequestDTO

--- src/main/java/sanguino/board/dtos/request/UserCreateRequestDto.java
+++ src/main/java/sanguino/board/dtos/request/UserCreateRequestDto.java
@@ -3,6 +3,7 @@ package sanguino.board.dtos.request;
 public class UserCreateRequestDto {
     private String nick;
     private String email;
+    private String password;
 
     public UserCreateRequestDto() {
     }
@@ -21,5 +22,13 @@ public class UserCreateRequestDto {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 }
