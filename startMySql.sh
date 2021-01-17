ef942b0530cb643e0a1286752f88784481c63517
Miguel G. Sanguino <sanguino@me.com>
2021-01-17 23:23:26 +0100

    fix startMysql.sh

--- startMySql.sh
+++ startMySql.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker run --rm -p 3306:3306 --name mysql-db -e MYSQL_ROOT_PASSWORD=pass -e MYSQL_DATABASE=test -e -d mysql:latest
+docker run --rm -d -p 3306:3306 --name mysql-db -e MYSQL_ROOT_PASSWORD=pass -e MYSQL_DATABASE=test mysql:latest
