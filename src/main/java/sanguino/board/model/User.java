43fc031a391e0b050f6f32694a2be2f8a66d4b98
Miguel G. Sanguino <sanguino@me.com>
2021-01-17 17:09:38 +0100

    added password to UserModel

--- src/main/java/sanguino/board/model/User.java
+++ src/main/java/sanguino/board/model/User.java
@@ -8,14 +8,16 @@ public class User {
     @Id
     private String nick;
     private String email;
+    private String password;
 
     public User() {
 
     }
 
-    public User(String nick, String email) {
+    public User(String nick, String email, String password) {
         this.nick = nick;
         this.email = email;
+        this.password = password;
     }
 
     public String getNick() {
@@ -34,12 +36,20 @@ public class User {
         this.email = email;
     }
 
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
     @Override
     public String toString() {
         return "User{" +
                 "nick='" + nick + '\'' +
                 ", email='" + email + '\'' +
-
+                ", password='" + password + '\'' +
                 '}';
     }
 }
